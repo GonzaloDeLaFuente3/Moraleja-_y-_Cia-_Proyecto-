ef88402100c0031ed62ceab4afaf0b60ea8d3bff
Kevin Aguero <91918754+kevinaguero@users.noreply.github.com>
2023-06-30 14:59:53 -0300

    config eliminado

--- moraleja/moraleja/configuraciones_locales.py
+++
@@ -1,8 +0,0 @@
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'moraleja',
-        "USER": 'postgres',
-        "PASSWORD": 'Player8744',
-    }
-}
